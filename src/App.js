5a7852ed9978af90f6b1833808108e5ace068bec
Ben Thorp <bthorp@bthorp-mac.lan>
2022-01-26 20:27:24 -0500

    heading and card layout copied from MUI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,110 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
 function App() {
+  const beers = [
+    {
+      name: 'elvis juice',
+      tagline: "it's good",
+      description: "it's a beer",
+      image: '',
+      abv: 10.5,
+      ibu: 5,
+    },
+    {
+      name: 'punk IPA',
+      tagline: "it's punky",
+      description: "it's an IPA",
+      image: '',
+      abv: 9.0,
+      ibu: 5,
+    },
+  ];
+
   return (
-    <div>
-      <h1>BrewDog Beers</h1>
-    </div>
+    <>
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            BrewDog Beers
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container>
+            <Typography
+              component="h1"
+              variant="h3"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              GREAT BEER THAT'S GREAT FOR THE PLANET
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="text.secondary"
+              paragraph
+            >
+              Exceptional quality is the cornerstone of our brewery. All BrewDog
+              beers are brewed using the finest malted barley and hops from the
+              best producers around the world.
+            </Typography>
+          </Container>
+        </Box>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {beers.map((beer) => (
+              <Grid item key={beer} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{
+                    height: '100%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      pt: '56.25%',
+                    }}
+                    image="https://source.unsplash.com/random"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe
+                      the content.
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+    </>
   );
 }
 
